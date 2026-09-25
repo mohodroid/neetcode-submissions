3f9670f64ca2f44850276385ddccea8c11606703
Sep <mohsen.mirtalleby@gmail.com>
2026-09-25 12:27:04 +0200

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.kt
@@ -0,0 +1,32 @@
+class Solution {
+
+    fun isPalindrome(s: String): Boolean {
+        val array = mutableListOf<Char>()
+
+        // O(n)
+        for(i in 0 until s.length) {
+            val char = s[i]
+            if (char in 'a'..'z' || char in 'A'..'Z' || char.isDigit()) {
+                array.add(char.lowercaseChar())
+            }
+        }
+        if(array.size == 0) {
+            return true
+        }
+
+        var end = array.size - 1 
+        for(i in 0 until array.size) {
+            if(array.get(i) == array.get(end)) {
+                end--
+            } else {
+                return false
+            }
+            if(i > end) {
+                return true
+            }
+            
+        }
+        return false
+    }
+
+}
